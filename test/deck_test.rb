54a85ac31b9f8968d0ab97f3acbe08c4dc2d5336
geoffreyadebonojo <38126942+geoffreyadebonojo@users.noreply.github.com>
2018-08-18 22:17:45 -0600

    Add test calculate unique card values method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -47,6 +47,18 @@ class DeckTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  
+  def test_calculates_correct_values
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("10", "Clubs")
+    card_3 = Card.new("Jack", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    deck = Deck.new([card_1, card_2, card_3, card_4])
+
+    assert_equal 16, deck.calculate_absolute_value(card_1)
+    assert_equal 10, deck.calculate_absolute_value(card_2)
+    assert_equal 37, deck.calculate_absolute_value(card_3)
+    assert_equal 53, deck.calculate_absolute_value(card_4)
+  end
+
 
 end
